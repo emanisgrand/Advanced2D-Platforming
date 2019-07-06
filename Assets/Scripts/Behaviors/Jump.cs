4a992d1cf7588eda752839a545f2fbec194eee3c
eman <tenhitcombo@gmail.com>
2019-07-06 17:15:20 -0400

    limit jump so it's not continuous

--- Assets/Scripts/Behaviors/Jump.cs
+++ Assets/Scripts/Behaviors/Jump.cs
@@ -14,11 +14,13 @@ public class Jump : AbstractBehavior
     // Update is called once per frame
     void Update() {
         var canJump = inputState.GetButtonValue(inputButtons[0]);
-
+        var holdTime = inputState.GetButtonHoldTime(inputButtons[0]);
+        
         if (collisionState.standing) {
-            if (canJump){
-                OnJump();
-            }
+            if (canJump && holdTime < .1f){      // the reason we don't test for 0 is-a small amount of time takes place 
+                OnJump();                         //from when the button is detected and changed on the input state, 
+                print(inputState.GetButtonHoldTime(0));                  //to when the jump script actually executes.
+            }                                    
         }
     }
     protected virtual void OnJump(){
--- Assets/Scripts/Input/InputState.cs
+++ Assets/Scripts/Input/InputState.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 // keep track of any of the button states
 public class ButtonState{
     public bool value; // sees if the button is pressed or not
+    public float holdTime = 0;
 }
 
 public enum  Directions{
@@ -19,9 +20,9 @@ public class InputState : MonoBehaviour
     // Let's keep an eye on what's actually going on with the rigidbody, and its velocity
     public float absVelX = 0f;
     public float absVelY = 0f;
+    
     private Rigidbody2D body2D;
-
-    public Dictionary<Buttons, ButtonState> buttonStates = new Dictionary<Buttons, ButtonState>();  // takes key: buttons enum and the value we created on line 6. instantiated in line
+    private Dictionary<Buttons, ButtonState> buttonStates = new Dictionary<Buttons, ButtonState>();  // takes key: buttons enum and the value we created on line 6. instantiated in line
     
     private void Awake() {
         body2D = GetComponent<Rigidbody2D>();
@@ -33,7 +34,7 @@ public class InputState : MonoBehaviour
         //value I will know whether it's greater than zero or if it's at rest, 
         //which would equal zero.
         absVelX = Mathf.Abs(body2D.velocity.x);
-        absVelX = Mathf.Abs(body2D.velocity.y);
+        absVelY = Mathf.Abs(body2D.velocity.y);
     }
 
     void Update() {
@@ -48,23 +49,29 @@ public class InputState : MonoBehaviour
 
         var state = buttonStates [key];  // sets reference to the key from the dictionary to a variable we will use ecalled state.
 
-        // if (state.value && !value) {
-        //     
-        // } else if (state.value && value) {
-        //     
-        // }
+        if (state.value && !value) {
+            state.holdTime=0;
+        } else if (state.value && value) {
+            state.holdTime += Time.deltaTime;
+        }
 
         //TODO:🔬 I think this is how the values are being set.
         state.value = value;               
     }
 
-    public bool GetButtonValue(Buttons key){  
-        if (buttonStates.ContainsKey(key)){       // if (or once) the dictionary has a key in it, 
+    public bool GetButtonValue(Buttons key) {  
+        if (buttonStates.ContainsKey (key))       // if (or once) the dictionary has a key in it, 
             return buttonStates [key].value;      // this bool returns its value... to the console(🔬) I'm not sure what the purpose of this is.
-        } else 
+         else 
             return false;
     }
-        
+
+    public float GetButtonHoldTime(Buttons key) {
+        if (buttonStates.ContainsKey (key))
+            return buttonStates [key].holdTime;
+         else
+            return 0; 
+    }
 }
 
 
