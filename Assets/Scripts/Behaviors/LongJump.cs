4cc6cbf5b4bee546c27b9b1082cfe9e0fc540633
eman <tenhitcombo@gmail.com>
2019-07-06 23:22:37 -0400

    longjump logic added to .cs

--- Assets/Scripts/Behaviors/LongJump.cs
+++ Assets/Scripts/Behaviors/LongJump.cs
@@ -7,6 +7,7 @@ public class LongJump : Jump
     public float longJumpDelay      = 0.15f;
     public float longJumpMultiplier = 1.5f;
     public bool canLongJump;
+    public bool isLongJumping;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,29 @@ public class LongJump : Jump
 
     // Update is called once per frame
     override protected void Update() {
+        // copied from Jump.cs
+        var canJump = inputState.GetButtonValue(inputButtons[0]);
+        var holdTime = inputState.GetButtonHoldTime(inputButtons[0]);
         
+        if (!canJump)
+            canLongJump = false; // can't long jump if you can't jump
+
+        if (collisionState.standing && isLongJumping)
+            isLongJumping = false;    
+
+        base.Update();  // calls all other logic created in Jump.cs
+
+        if (canLongJump && !collisionState.standing && holdTime > longJumpDelay){
+            var vel = body2D.velocity;
+            body2D.velocity = new Vector2 (vel.x, jumpSpeed * longJumpMultiplier);
+            canLongJump = false;
+            isLongJumping = true;
+        }
     }
+
+    protected override void OnJump(){
+        base.OnJump();
+        canLongJump = true;      // resets canLongJump 
+    }
+
 }
