0bb376f509e656552d3727336b371e8190b546a2
eman <tenhitcombo@gmail.com>
2019-07-10 17:41:55 -0400

    Wall slide collision  detection

--- Assets/Scripts/Collision/CollisionState.cs
+++ Assets/Scripts/Collision/CollisionState.cs
@@ -6,14 +6,18 @@ public class CollisionState : MonoBehaviour
 {
     public LayerMask collisionLayer;  //the actual collision layer we'll be doing the tests with
     public bool standing;
+    public bool onWall;
     public Vector2 bottomPosition = Vector2.zero; // the position or the point in space we want to actually test for a collision
+     public Vector2 rightPosition = Vector2.zero;
+    public Vector2 leftPosition = Vector2.zero; 
     public float collisionRadius = 10f; // the distance around this point for us to test collision
     public Color debugCollisionColor = Color.red;
 
+    private InputState inputState;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Awake() {
+        inputState = GetComponent<InputState>();
     }
 
     // Update is called once per frame
@@ -30,15 +34,29 @@ public class CollisionState : MonoBehaviour
         //and the radius that we're defining up here and see if it's within 
         //this collisionLayer. If it is, it'll return true. If not, it'll return false.
         standing = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
+
+        pos = inputState.direction == Directions.Right ? rightPosition : leftPosition;  
+        pos.x += transform.position.x;
+        pos.y += transform.position.y;
+
+        onWall = Physics2D.OverlapCircle(pos, collisionRadius, collisionLayer);
+
     }
 
     private void OnDrawGizmos() {
         Gizmos.color = debugCollisionColor;
-        var pos = bottomPosition;  
-        pos.x += transform.position.x; 
-        pos.y += transform.position.y; 
 
-        Gizmos.DrawWireSphere(pos, collisionRadius);
+        var positions = new Vector2[] {rightPosition, bottomPosition, leftPosition};
+
+        foreach (var position in positions) {
+            var pos = position;
+            pos.x += transform.position.x;
+            pos.y += transform.position.y;
+            Gizmos.DrawWireSphere(pos, collisionRadius);
+        }
+        
+
+        
     }
 
 }
