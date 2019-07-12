c9384688117623549e6db5d19448a90f140061f3
eman <tenhitcombo@gmail.com>
2019-07-12 12:16:45 -0400

    wallslide acceleration

--- Assets/Scripts/Behaviors/WallSlide.cs
+++ Assets/Scripts/Behaviors/WallSlide.cs
@@ -4,13 +4,22 @@ using UnityEngine;
 
 public class WallSlide : StickToWall
 {
-    public float slideVelocity = -5;
+    public float slideVelocity = -5f;
+    // we want to increase the value of the slide velocity
+    // so we create a multiplier to do that.
+    public float slideMultiplier = 5f;
+    
 
     override protected void Update() {
         base.Update();
 
         if(onWallDetected) {
             var velY = slideVelocity;
+
+            // TODO: we always use the first input in the array. I'm not sure why
+            // I believe it's because we set the input in the inspector. In this case: down
+            if (inputState.GetButtonValue(inputButtons[0]))
+                velY *= slideMultiplier;  // multiply the slide velocity. So -5*5=-25 which will speed it up.
 
             body2D.velocity = new Vector2(body2D.velocity.x, velY);
         }
