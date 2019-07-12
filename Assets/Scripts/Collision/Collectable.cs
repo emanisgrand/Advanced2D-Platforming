d75edf457162600f276aea10629dcb28861a6d8d
eman <tenhitcombo@gmail.com>
2019-07-12 15:14:12 -0400

    +Collectable: PowerUpFlower.cs scripts

---
+++ Assets/Scripts/Collision/Collectable.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public string targetTag = "Player";
+
+    void OnTriggerEnter2D(Collider2D target) {
+        if (target.gameObject.tag == targetTag) {  // checking if its colliding with the targetag
+            OnCollect(target.gameObject);  //run the OnCollect function
+            OnDestroy();
+        }    
+    }
+    
+    protected virtual void OnCollect(GameObject target){
+
+    }
+
+    protected virtual void OnDestroy() {
+        Destroy(gameObject);
+    }
+}
