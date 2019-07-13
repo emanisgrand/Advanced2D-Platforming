d698462f648dd2a4c43df78b04ba9c90257d0385
eman <tenhitcombo@gmail.com>
2019-07-13 17:41:16 -0400

    +powerupflower.cs

---
+++ Assets/Scripts/Items/FireBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBall : MonoBehaviour {
+    public Vector2 initialVelocity = new Vector2 (100,-100);
+    
+    private Rigidbody2D body2D;
+
+    private void Awake() {
+        body2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+
+        var startVelX = initialVelocity.x * transform.localScale.x;
+        // This is how the fireball will move once it's instantiated.
+        body2D.velocity = new Vector2 (startVelX, initialVelocity.y);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
--- Assets/Scripts/Items/PowerUpFlower.cs
+++ Assets/Scripts/Items/PowerUpFlower.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class PowerUpFlower : Collectable
 {
     // this is the item ID for the flower itself.
-    public int itmeID = 1;
+    public int itemID = 1;
     // the parent class is empty so there's no need to call base.OnCollect here.
     override protected void OnCollect(GameObject target){
         
         var equipBehavior = target.GetComponent<Equip>();
         
         if (equipBehavior != null){
-            equipBehavior.currentItem = itmeID;
+            equipBehavior.currentItem = itemID;
         }
     }
 }
